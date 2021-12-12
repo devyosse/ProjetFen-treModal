160d18500af90be9c7ffd52f6cabe4a12d2e9029
devyosse <narutoshippuden884@gmail.com>
2021-12-12 14:58:16 +0100

    projetFenêtre

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ let close = document.getElementById("close");
 let modal = document.getElementById("MyModal");
 
 
+let Fen = (modal)
+{
+   Fen.style.display = "none";
+}
+
 MyBtn.addEventListener("click",  () => {
     modal.style.display = "block";
 })
