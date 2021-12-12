532e35e83159aabe57ed676458fb20fb5a82d8ec
devyosse <narutoshippuden884@gmail.com>
2021-12-12 16:03:37 +0100

    projetFenêtre

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 let MyBtn = document.getElementById('MyBtn');
 let close = document.getElementById("close");
 let modal = document.getElementById("MyModal");
+let add = document.getElementById('add');
 
 
-let Fen = (modal)
-{
-   Fen.style.display = "none";
+let Fenetre = (modal) => {
+    this.modal = modal;
+    modal.style.display = "block";
+
+    MyBtn.addEventListener("click", () => {
+        modal.style.display = "block";
+    })
+
+    close.addEventListener("click", () => {
+        modal.style.display = "none";
+    })
+
+
+    add.addEventListener("click", () => {
+        moddal2.style.display = "block";
+    })
+
 }
 
-MyBtn.addEventListener("click",  () => {
-    modal.style.display = "block";
-})
 
-close.addEventListener("click", () => {
-    modal.style.display = "none";
-})
 
+let nouveau = function (modal2) {
+    Fenetre.call(this, modal, modal2);
+};
+
+nouveau.prototype = Object.create(Fenetre);
+nouveau.prototype.constructor = nouveau;
+nouveau.prototype.modd =function (){
+    if (this.modal2){
+        return moddal2.style.display = "block";
+    }
+}
+let moddal2 = new nouveau("modal2");
+Fenetre(modal, nouveau);
